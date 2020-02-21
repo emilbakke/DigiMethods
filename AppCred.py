b035642135c042db531ef50be09be8ff23b6bc63
emilbakke <emilbakke@users.noreply.github.com>
2020-02-21 12:35:58 +0100

    Update

---
+++ AppCred.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Application credentials for my Twitter API
+
+CONSUMER_KEY = "GpRFBhpRM9A09REzPYf79cfnc"
+CONSUMER_SECRET = "7sD24FtaNJrG2nvB8meDrjbRsB0zRx8KvPLaZOzvRhsEN6QY43"
+
+ACCESS_TOKEN = "65910744-jddiHvj3LlLKXYFeGZIRbcEJF8zOBb6Mz36y9wdVF"
+ACCESS_TOKEN_SECRET = "mB6asjHZquyd6E741Z2fK9IodxZ7CvQSQWrffHbXLf9Ep"
+
